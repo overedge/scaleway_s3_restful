cfbd883d593b07f02ef6f48f733804b41e10e152
overedge <overedge@MacBook-Air-de-overedge.local>
2017-12-24 02:48:52 +0100

    - Add tests in README  - Finished GET/DELETE/POST Image Endpoint and work fine ( Need refactoring ) go to sleep ...

--- README.md
+++ README.md
@@ -11,4 +11,11 @@
 ## SOME TESTS
 ```
 curl -X POST -F "description=mojo" -F "image=@./scaleway.png" -F "name=42" 127.0.0.1:5000
+curl -X POST -F "image=@./scaleway.png" -F "name=42" 127.0.0.1:5000
+curl -X POST -F "image=@./scaleway.png" 127.0.0.1:5000
+curl -X POST -F "description=mojo" 127.0.0.1:5000
+curl -X POST -F "description=mojo" -F "image=@./scaleway.png" 127.0.0.1:5000
+curl http://127.0.0.1:5000/image
+curl http://127.0.0.1:5000/image/1
+curl -X DELETE http://127.0.0.1:5000/image/1
 ```
--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import  os
 import  sqlite3
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restful import Resource, Api
 from werkzeug.utils import secure_filename
 from minio import Minio
@@ -17,11 +17,39 @@ api = Api(app)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 class Image(Resource):
-    def get(self):
-        return "ICI JE LISTE TOUT LES UPLOAD ou L'image en question si ID"
+    def get(self, id = None):
+        """
+            Endpoint  : GET /images || GET /images/<int:id>
+            Desc1     : List all images like data[id] = {name, description, URI}
+            Desc2     : Get image
+            Error     : Error if 'id' doesn't exist
+        """
+        if id == None:
+            cursor = bdd.cursor()
+            cursor.execute("SELECT * FROM images")
+            result = {'data': {}}
+            for row in cursor:
+                result['data'][row[0]] = {'name': row[2], 'description': row[3], 'uri': "http://127.0.0.1:5000/image/" + str(row[0])}
+            return result
+        cursor = bdd.cursor()
+        req = cursor.execute("SELECT imagename FROM images where id = ?", (id,))
+        data = cursor.fetchone()
+        if data == None:
+            return {'error': "This id doesn't match with any image"}
+        stream = minioClient.get_object(BUCKET_NAME, data[0])
+        extension = data[0].rsplit('.', 1)[1].lower()
+        response = app.make_response(stream.read())
+        response.mimetype = "image/" + extension
+        return response
 
     def post(self):
+        """
+            Endpoint : POST /image
+            Desc     : Add image, name, description (optional) in database and in bucket
+            Error    : Error if filename exist or bad filename or bad extention
+        """
         if 'image' not in request.files or 'name' not in request.form:
             return {'error': "No file part or No 'name' paramters"}
         else:
@@ -40,22 +68,42 @@ class Image(Resource):
                     minioClient.put_object(BUCKET_NAME, imagename, data, size, image.content_type)
                     os.remove(os.path.join(UPLOAD_FOLDER, imagename))
                     cursor = bdd.cursor()
-                    cursor.execute("INSERT INTO images (name, description) VALUES (?, ?)", (name, description))
+                    cursor.execute("INSERT INTO images (imagename, name, description) VALUES (?, ?, ?)", (imagename, name, description))
                     last_id = cursor.lastrowid
                     bdd.commit()
-                    return {'success': 'http://localhost:5000/' + str(last_id)}
+                    return {'success': 'http://127.0.0.1:5000/image/' + str(last_id)}
             return {'error': 'Format allowed is .png .jpg .jpeg .gif or bad filename'}
 
-    def delete(self):
-        return "ICI JE SUPRIME UNE PHOTO"
+    def delete(self, id):
+        """
+            Endpoint : DELETE /image/<int:id>
+            Desc     : Delete image from 'id' in database and in bucket
+            Error    : Error if 'id' doesn't exist
+        """
+        cursor = bdd.cursor()
+        req = cursor.execute("SELECT imagename FROM images where id = ?", (id,))
+        data = cursor.fetchone()
+        if data == None:
+            return {'error': "This id doesn't match with any image"}
+        cursor.execute("DELETE FROM images where id = ?", (id,))
+        bdd.commit()
+        minioClient.remove_object(BUCKET_NAME, data[0])
+        return {'success': 'Image has successfully deleted'}
 
-api.add_resource(Image, '/')
+api.add_resource(Image, '/image', '/image/<int:id>', endpoint="image_get_delete")
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     bdd = sqlite3.connect('s3.db', check_same_thread=False)
-    bdd.execute("CREATE TABLE IF NOT EXISTS images ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT  NOT NULL, `description` TEXT DEFAULT NULL);")
+    bdd.execute("""
+    CREATE TABLE IF NOT EXISTS images(
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+            `imagename` TEXT  NOT NULL,
+            `name` TEXT  NOT NULL,
+            `description` TEXT DEFAULT NULL
+        );
+    """)
     minioClient = Minio(
         '127.0.0.1:9000',
         access_key='SCALEWAYS3LIKE',
